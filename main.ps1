94eed759786409cf7a61071a81aecd63a787ec53
olebru <46709018+olebru@users.noreply.github.com>
2021-10-27 07:42:26 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -3,20 +3,16 @@ Param(
     $eventRecordID
 )
 $whitelist = (get-content -path .\whitelist.txt).split(',')
-
 $theevent = Get-EventLog -LogName $eventChannel -Index $eventRecordID
 $sourceip = $theevent.Message.Substring( $theevent.Message.IndexOf("Source Network Address:"), 50).split([string]::Empty)[3]
-
 $account = $theevent.Message.Substring( $theevent.Message.IndexOf("Account For Which Logon Failed:"),100).split([string]::Empty)[16]
-
-
 "---  $eventChannel $eventRecordID $sourceip $account---"| Out-File -FilePath C:\temp\log.txt -Append
-
 if(!$whitelist.contains($account))
 {
    "Creating Firewallrule..." | Out-File -FilePath C:\temp\log.txt -Append
-   New-NetFirewallRule -DisplayName "Block scriptkiddie src ip..." -Direction Inbound -Action Block -RemoteAddress $sourceip
-    
-}else {
+   New-NetFirewallRule -DisplayName "Block scriptkiddie src ip..." -Direction Inbound -Action Block -RemoteAddress $sourceip  
+}
+else 
+{
    "Ignoring $account " | Out-File -FilePath C:\temp\log.txt -Append
 }
