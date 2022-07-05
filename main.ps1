46969cd99838cb4c63af8fbdc371ff317173f7f4
Ole I Bruvik <oleib@me.com>
2022-07-05 12:13:12 +0200

    Changed to get-winevent (legacy cmdlet did not handle 32bit+ ids

--- main.ps1
+++ main.ps1
@@ -7,10 +7,14 @@ if($null -eq $whitelist)
 {
    $whitelist = [System.Collections.Generic.List[psobject]]::New()
 }
-$theevent = Get-EventLog -LogName $eventChannel -Index $eventRecordID
+$theevent = Get-WinEvent -LogName $eventChannel | where recordid -eq $eventRecordID  
 $sourceip = $theevent.Message.Substring( $theevent.Message.IndexOf("Source Network Address:"), 50).split([string]::Empty)[3]
 $account = $theevent.Message.Substring( $theevent.Message.IndexOf("Account For Which Logon Failed:"),100).split([string]::Empty)[16]
 if(!$whitelist.contains($account))
 {
-   New-NetFirewallRule -DisplayName "Block scriptkiddie that tried log in as: $account" -Direction Inbound -Action Block -RemoteAddress $sourceip
+	if($null -ne $sourceip -and $null -ne $account)
+		{
+			New-NetFirewallRule -DisplayName "Block scriptkiddie that tried log in as: $account" -Direction Inbound -Action Block -RemoteAddress $sourceip
+		}
+
 }
